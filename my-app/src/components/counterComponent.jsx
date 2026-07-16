1ec85b3a7aa6b9ff43e356b5df2ed17892622fea
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-07-16 09:13:30 +0600

    take input from user and update it

---
+++ my-app/src/components/counterComponent.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function counter(){
+    const initialValue =0;
+    const [count ,setCount] =useState(initialValue);
+    return(
+        <div>
+            <h2>{count}</h2>
+            <button onClick={() =>
+             setCount(count+1)} >Increment</button>
+              <button onClick={() =>
+             setCount(count-1)} >Decrement</button>
+        </div>
+    );
+}
