6841f0852cb0f87bd9b2ae28092e156a13864c6f
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-07-17 21:11:47 +0600

    add reste button,return 0

--- my-app/src/State/counterComponent.jsx
+++ my-app/src/State/counterComponent.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function counter(){
     const initialValue =0;
     const [count ,setCount] =useState(initialValue);
+    
     return(
         <div>
             <h2>{count}</h2>
@@ -10,6 +11,9 @@ function counter(){
              setCount(count+1)} >Increment</button>
               <button onClick={() =>
              setCount(count-1)} >Decrement</button>
+             <button onClick={() =>
+             setCount(0)} >Reset</button>
+
         </div>
     );
 }
