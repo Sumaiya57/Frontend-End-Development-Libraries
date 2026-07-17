93eff870ded068b2a1e368144f7dd84f652faf70
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-07-17 11:34:24 +0600

    reduced space complexity and easy to maintain

--- my-app/src/State/MoreState.jsx
+++ my-app/src/State/MoreState.jsx
@@ -4,18 +4,12 @@ function Profile(){
     
     const [name, setName] = useState("");
     const [age, setAge] = useState("");
-function handleNameChange(event){
-        setName(event.target.value);
-    }
-function handleAgeChange(event){
-        setAge(event.target.value);
-    }
 
     return (
     <div>
 
-      <input placeholder="give your name"  value={name} onChange={handleNameChange} />
-      <input placeholder="give your Age"  value={age} onChange={handleAgeChange}  />
+      <input placeholder="give your name"  value={name} onChange={(e) => setName(e.target.value)} />
+      <input placeholder="give your Age"  value={age} onChange={(e) => setAge(e.target.value)}  />
       <p>Name:{name}, Age:{age}</p>
 
     </div>
