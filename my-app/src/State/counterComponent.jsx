2fcb7ec209c679835675472b4bd7a58fed990c94
sumaiya57 <sumaiyakamal2002@gmail.com>
2026-07-17 21:27:50 +0600

    add validation so that count>1

--- my-app/src/State/counterComponent.jsx
+++ my-app/src/State/counterComponent.jsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 function counter(){
     const initialValue =0;
     const [count ,setCount] =useState(initialValue);
-    
+    function handleDecrease() {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  }
+
     return(
         <div>
-            <h2>{count}</h2>
+            <h2>Quantity:{count}</h2>
             <button onClick={() =>
              setCount(count+1)} >Increment</button>
-              <button onClick={() =>
-             setCount(count-1)} >Decrement</button>
+            <button onClick={handleDecrease} >Decrement</button>
              <button onClick={() =>
              setCount(0)} >Reset</button>
 
